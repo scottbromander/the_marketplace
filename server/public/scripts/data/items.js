c5858008d150d8c6c4ed941843e566434d8ecf48
Scott Bromander <scott@primeacademy.io>
2017-04-21 20:44:47 -0500

    update

--- server/public/scripts/data/items.js
+++ server/public/scripts/data/items.js
@@ -1,50 +1,50 @@
 const GAME_ITEMS = [
     {
-      "name" : "Apples",
-      "type" : "FruitItem"
+      'name' : 'Apples',
+      'type' : 'FruitItem'
     },
     {
-      "name" : "Bananas",
-      "type" : "FruitItem"
+      'name' : 'Bananas',
+      'type' : 'FruitItem'
     },
     {
-      "name" : "Oranges",
-      "type" : "FruitItem"
+      'name' : 'Oranges',
+      'type' : 'FruitItem'
     },
     {
-      "name" : "Grapes",
-      "type" : "FruitItem"
+      'name' : 'Grapes',
+      'type' : 'FruitItem'
     },
     {
-      "name" : "Toasters",
-      "type" : "ElectronicItem"
+      'name' : 'Toasters',
+      'type' : 'ElectronicItem'
     },
     {
-      "name" : "Lamps",
-      "type" : "ElectronicItem"
+      'name' : 'Lamps',
+      'type' : 'ElectronicItem'
     },
     {
-      "name" : "Clocks",
-      "type" : "ElectronicItem"
+      'name' : 'Clocks',
+      'type' : 'ElectronicItem'
     },
     {
-      "name" : "BluRay Players",
-      "type" : "ElectronicItem"
+      'name' : 'BluRay Players',
+      'type' : 'ElectronicItem'
     },
     {
-      "name" : "Comic Books",
-      "type" : "CollectableItem"
+      'name' : 'Comic Books',
+      'type' : 'CollectableItem'
     },
     {
-      "name" : "Fancy Stuffed Animals",
-      "type" : "CollectableItem"
+      'name' : 'Fancy Stuffed Animals',
+      'type' : 'CollectableItem'
     },
     {
-      "name" : "Jewelry",
-      "type" : "CollectableItem"
+      'name' : 'Jewelry',
+      'type' : 'CollectableItem'
     },
     {
-      "name" : "Wine",
-      "type" : "CollectableItem"
-    },
+      'name' : 'Wine',
+      'type' : 'CollectableItem'
+    }
 ];
--- server/public/scripts/data/settings.js
+++ server/public/scripts/data/settings.js
@@ -1,7 +1,7 @@
 const PLAYER_STARTING_CASH = 100;                           // IN WHOLE DOLLARS
 const ITEM_PRICE_SWING     = 50;                            // IN WHOLE CENTS
 const ITEM_STARTING_COST   = 5;                             // IN WHOLE DOLLARS
-const TIMER_INTERVAL       = 100;                           // IN MILLISECONDS
+const TIMER_INTERVAL       = 15000;                         // IN MILLISECONDS
 const LOWEST_PRICE         = 0.5;                           // DOLLARS AND CENTS
 const HIGHEST_PRICE        = 9.99;                          // DOLLARS AND CENTS
 const GAME_TIME_MINUTES    = 15;                            // IN MINUTES
